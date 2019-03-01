3e75dfa59ab8fde7ef4de28d6b825239db51cc52
rjames91 <robert.james@manchester.ac.uk>
2019-03-01 10:16:36 +0000

    added BF vector

--- MAP_vocoder_data_pack.py
+++ MAP_vocoder_data_pack.py
@@ -7,7 +7,7 @@ from scipy.io import savemat, loadmat
 from elephant.statistics import isi,cv
 
 # Open the results
-n_fibres = 3000
+n_fibres = 300
 results_directory = '/home/rjames/Dropbox (The University of Manchester)/EarProject/Pattern_recognition/spike_trains/IC_spikes'
 results_file = "/cn_timit_1s_{}an_fibres_0.1ms_timestep_50dB_lateralTrue.npz".format(n_fibres)
 
@@ -17,6 +17,9 @@ an_spikes = results_data['an_spikes']
 wav_directory = '../OME_SpiNN/'
 dBSPL = 50.
 Fs = 50e3
+
+max_power = min([np.log10(Fs / 2.), 4.25])
+BFs = np.logspace(np.log10(30), max_power, int(n_fibres/10.))
 
 timit_l = generate_signal(signal_type='file',dBSPL=dBSPL,fs=Fs,ramp_duration=0.0025,silence=True,
                             file_name=wav_directory+'10788.wav',plt=None,channel=0)
@@ -33,7 +36,17 @@ results_dict['dBSPL']=dBSPL
 results_dict['Fs']=Fs
 results_dict['audio_stimulus']=timit
 results_dict['an_spikes']=an_spikes
-results_dict['moc_att']=moc_att
+moc_att_resampled =[]
+for i,moc_ear in enumerate(moc_att):
+    moc_att_resampled.append([])
+    for moc in moc_ear:
+        moc_att_resampled[i].append(resample(moc,100.,Fs))
+
+# plt.figure()
+# plt.plot(moc_att_resampled[0][0])
+# plt.show()
+results_dict['moc_att']=moc_att_resampled
+results_dict['BFs']=BFs
 
 
 savemat(results_directory+'/{}_fibres_results'.format(n_fibres),results_dict)
